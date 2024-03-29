b063147c246e0910fe9b45c60852cba2973e0d5c
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-03-29 11:06:17 +0100

    feat: add format_instructions

--- src/app.py
+++ src/app.py
@@ -1,8 +1,11 @@
 import chainlit as cl
 from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.schema import StrOutputParser
 
 # local
 from prompt import prompt
+from schema import Invoice
 
 
 @cl.on_chat_start
@@ -15,6 +18,12 @@ async def on_chat_start():
 @cl.on_message
 async def on_message(message: cl.Message):
     model = ChatOpenAI(streaming=True)
-    chain = prompt | model
-    result = chain.invoke({"question": message.content})
+    parser = JsonOutputParser(pydantic_model=Invoice)
+    chain = prompt | model | StrOutputParser()
+    result = chain.invoke(
+        {
+            "question": message.content,
+            "format_instructions": parser.get_format_instructions(),
+        }
+    )
     await cl.Message(content=result).send()
--- src/prompt.py
+++ src/prompt.py
@@ -1,7 +1,17 @@
 from langchain.prompts.chat import ChatPromptTemplate
 
-human_template = """
-  {question}
+system_template = """
+  Hello,
+  I'm your virtual assistant Lara and I do the bookkeeping for you.
+  Should I write an invoice or post an incoming or outgoing invoice?
 """
 
-prompt = ChatPromptTemplate.from_messages([("human", human_template)])
+human_template = """
+  Fill in None if you don't know the answer.
+  {question}
+  {format_instructions}
+"""
+
+prompt = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("human", human_template)]
+)
